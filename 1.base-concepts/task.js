c26f7085a3f8962553c15c968c34a40e1b817681
Natalie210985 <bns_natalie@mail.ru>
2023-04-22 21:58:14 +0300

    Првое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,30 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a *c;
+  if (discriminant === 0) {
+    let discriminantSquare = -b / (2 * a);
+    arr.push(discriminantSquare);
+  }
+  else if (discriminant > 0) {
+    let discriminantSquare1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let discriminantSquare2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(discriminantSquare1, discriminantSquare2);
+  }  
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(persent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
+    return false
+  }
+  let monthPercent = (percent / 100) / 12;
+  let creditBody = amount - contribution;
+  let monthPayment = creditBody * (monthPercent + (monthPercent / (((1 + monthPercent) ** countMonths)-1)));
+  let payment = monthPayment * countMonths;
+  if (payment != 0) {
+    payment = parseFloat(payment.toFixed(2))
+  }
+  return payment;
 }
